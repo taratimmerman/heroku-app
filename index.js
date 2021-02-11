3b75d9287a6dff568694ae6dbecb2eb4b2cf32ec
taratimmerman <timmermantara@gmail.com>
2021-02-11 11:04:59 -0700

    second push

---
+++ index.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv').config()
+const express = require("express")
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/*', (_,res) => {
+    res.json({data: "The API lives!!!"})
+})
+
+app.listen(port, ()=> {
+    console.log(`Server is alive on port ${port}`)
+})
+
+// console.log("It's alive!")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME)
+// console.log(process.env.PORT)
